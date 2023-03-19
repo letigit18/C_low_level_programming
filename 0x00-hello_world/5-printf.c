ebf0b8931e41b66a8669acb4c0742ee7bdd8bffd
mersigit <mersi.tesfaye18@gmail.com>
2023-03-19 05:55:32 -0700

    demonstration of printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - function main
+ * description - beginning of the execution
+ * Return: (0) returns 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
