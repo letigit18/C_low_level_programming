05a306d986c90c1124110c0d02b9f42e96b95a3b
mersigit <mersi.tesfaye18@gmail.com>
2023-03-19 07:04:19 -0700

    UNIX fputs function

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - where the execution of the program starts
+ * description - the main function
+ * Return: 1 (sucess)
+ */
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\r", stdout);
+	return (1);
+}
