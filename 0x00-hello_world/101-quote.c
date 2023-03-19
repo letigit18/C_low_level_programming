31875eb642efdf236824dc6e5a7528bea52cad37
mersigit <mersi.tesfaye18@gmail.com>
2023-03-19 07:07:04 -0700

    unix fput function

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\r", stdout);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\r", stdout);
 	return (1);
 }
