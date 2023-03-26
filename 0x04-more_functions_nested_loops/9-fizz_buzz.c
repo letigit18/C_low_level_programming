b8d95059514f7d1e007fe3652f9aea530369c7fb
mersigit <mersi.tesfaye18@gmail.com>
2023-03-26 10:34:21 -0700

    printing Fuzz Bizz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - the execution of the program starts here
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 != 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0 && i % 3 != 0)
+		{
+			printf("Buzz ");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else
+		{
+		printf("%d", i);
+		}
+		printf(" ");
+	}
+	return (0);
+}
