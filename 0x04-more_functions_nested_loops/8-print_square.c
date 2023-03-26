49f5266a813f56f3c77fb4f8f954fd625599c0c2
mersigit <mersi.tesfaye18@gmail.com>
2023-03-26 09:53:24 -0700

    print square diagram

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square - prints the square diagram
+ * @size: the parameter passed to the function
+ */
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+		int j;
+
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
