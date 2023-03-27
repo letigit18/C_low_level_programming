ffc566756806bbfd74dc12f3dbeb64d8432f345d
mersigit <mersi.tesfaye18@gmail.com>
2023-03-27 11:51:04 -0700

    6-puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * puts2 - function should print only one character out of two
+ * @str: string parameter
+ */
+void puts2(char *str)
+{
+	int longi = 0;
+	int t = 0;
+	char x = str;
+	int o;
+
+	while (*x != '\0')
+	{
+		x++;
+		longi++;
+	}
+	t = longi - 1;
+	for (o = 0 ; o <= t ; o++)
+	{
+		if (o % 2 == 0)
+	{
+		_putchar(str[o]);
+	}
+	}
+	_putchar('\n');
+}
+
