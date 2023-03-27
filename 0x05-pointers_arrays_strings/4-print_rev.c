5c5ae75eb3505d27a2f58c46d07665fcc7df689f
mersigit <mersi.tesfaye18@gmail.com>
2023-03-27 11:32:31 -0700

    4-print_rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,6 +1,23 @@
 #include "main.h"
-
+/**
+ * print_rev - print the reverse of characters
+ * @s: the string passed to the function
+ */
 void print_rev(char *s)
 {
-	
+	int longi = 0;
+	int o;
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+	s--;
+	for (o = longi; o > 0; o--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
 }
