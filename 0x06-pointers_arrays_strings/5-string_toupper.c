be5f11b39a17c0cebb1180d7e0e53ca67994a008
mersigit <mersi.tesfaye18@gmail.com>
2023-03-30 11:18:53 -0700

    5-string_toupper updated

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * string_toupper - the function that converts characters
+ * @n: the string parameter
+ * Return: converted character
+ */
+char *string_toupper(char *n)
+{
+	int i;
+
+	i = 0;
+	while (n[i] != '\0')
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+		{
+			n[i] = n[i] - 32;
+		}
+		i++;
+	}
+	return (n);
+}
+
