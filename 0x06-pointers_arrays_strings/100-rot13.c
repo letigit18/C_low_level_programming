faf6ddcf0f97ebeaa8ccb367e5264d1b74b79be4
mersigit <mersi.tesfaye18@gmail.com>
2023-03-30 11:42:50 -0700

    100-rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rot13 - encoder rot13
+ * @s: pointer to string params
+ *
+ * Return: *s
+ */
+
+char *rot13(char *s)
+{
+	int index;
+	int j;
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char data2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (index = 0; s[index] != '\0'; index++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[index] == data1[j])
+			{
+				s[index] = data2[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
+
