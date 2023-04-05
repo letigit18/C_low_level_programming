62183602ffc7c8f8df116fbd230500f91835dd0a
mersigit <mersi.tesfaye18@gmail.com>
2023-04-05 02:15:53 -0700

    print strings in reverse order

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -5,19 +5,9 @@
  */
 void _print_rev_recursion(char *s)
 {
-	int n;
-	int i;
-
-	n = 0;
-	i = 0;
-	if (*s != '\0')
+	if (*s)
 	{
-		n++;
 		_print_rev_recursion(s + 1);
-	}
-	if (i <= n)
-	{
-		_putchar(s[n]);
-		n--;
+		_putchar(*s);
 	}
 }
