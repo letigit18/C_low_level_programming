ac9203928360ee4d03c3557756524a8c77fab137
mersigit <mersi.tesfaye18@gmail.com>
2023-04-05 06:47:43 -0700

    4-pow_recursion.c updated

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * _pow_recursion - the function that calculates recursion
+ * @x: the first parameter
+ * @y: exponent parameter
+ * Return: the calculated result
+ */
 int _pow_recursion(int x, int y)
 {
 	int pow;
