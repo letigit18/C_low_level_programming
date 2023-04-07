b477d7dd2ebada8392b00941551cf73709d48403
mersigit <mersi.tesfaye18@gmail.com>
2023-04-07 05:13:22 -0700

    multiplying two arg values done

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,14 +13,17 @@ int main(int argc, char *argv[])
 	int n1;
 	int n2;
 
-	if (argc < 2)
+	if (argc == 3)
 	{
-		printf("Error\n");
-		return (1);
-	}
 	n1 = atoi(argv[1]);
 	n2 = atoi(argv[2]);
 	mul = n1 * n2;
 	printf("%i\n", mul);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
 	return (0);
 }
