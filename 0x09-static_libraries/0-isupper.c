e95703e296f393c4a2c4af4ffc1825db7ea9848c
mersigit <mersi.tesfaye18@gmail.com>
2023-04-07 13:29:22 -0700

    static library creatin done

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isupper - the function that prints characters
+ * @c: the parameter passed to fun
+ * Return: 0 for success
+ */
+int _isupper(int c)
+{
+
+}
---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _putchar - the function that prints characters
+ * @c: the parameter passed to fun
+ * Return: 0 for success
+ */
+int _putchar(char c)
+{
+
+}
