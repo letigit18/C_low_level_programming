22b6070e15fa71a140a40180605582e7243ef6c7
mersigit <mersi.tesfaye18@gmail.com>
2023-04-11 00:08:12 -0700

    string concatenation done

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -10,32 +10,32 @@
 char *argstostr(int ac, char **av)
 {
 	int i, n;
-	char *p;
+        char *p;
 
-	r = 0;
-	l = 0;
-	if (ac == 0 || av == NULL)
-		return (NULL);
-	for (i = 0; i < ac; i++)
-	{
-		for (n = 0; av[i][n]; n++)
-			l++;
-	}
-	l += ac;
-	p = malloc(sizeof(char) * l + 1);
-	if (p == NULL)
-		return (NULL);
-	for (i = 0; i < ac; i++)
-	{
-	for (n = 0; av[i][n]; n++)
-	{
-		p[r] = av[i][n];
-		r++;
-	}
-	if (p[r] == '\0')
-	{
-		p[r++] = '\n';
-	}
-	}
-	return (p);
+        r = 0;
+        l = 0;
+        if (ac == 0 || av == NULL)
+                return (NULL);
+        for (i = 0; i < ac; i++)
+        {
+                for (n = 0; av[i][n]; n++)
+                        l++;
+        }
+        l += ac;
+        p = malloc(sizeof(char) * l + 1);
+        if (p == NULL)
+                return (NULL);
+        for (i = 0; i < ac; i++)
+        {
+        for (n = 0; av[i][n]; n++)
+        {
+                p[r] = av[i][n];
+                r++;
+        }
+        if (p[r] == '\0')
+        {
+                p[r++] = '\n';
+        }
+        }
+        return (p);
 }
