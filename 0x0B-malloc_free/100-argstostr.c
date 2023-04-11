6ec2e20067b3732ae7ee762f6a13080d25feecd7
mersigit <mersi.tesfaye18@gmail.com>
2023-04-10 23:45:07 -0700

    concatenation of string done

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - main entry
+ * @ac: int input
+ * @av: double pointer array
+ * Return: 0
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, n;
+	char *p;
+
+	r = 0;
+	l = 0;
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (n = 0; av[i][n]; n++)
+			l++;
+	}
+	l += ac;
+	p = malloc(sizeof(char) * l + 1);
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+	for (n = 0; av[i][n]; n++)
+	{
+		p[r] = av[i][n];
+		r++;
+	}
+	if (p[r] == '\0')
+	{
+		p[r++] = '\n';
+	}
+	}
+	return (p);
+}
