3101aafeec0adc90e54a3ed012206429af0592f2
mersigit <mersi.tesfaye18@gmail.com>
2023-04-20 01:55:50 -0700

    calculating the sum done

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include <stdarg.h>
+
+/**
+ * sum_them_all - the function that calcualtes the sum
+ * @n: number of arguments
+ * Return: the sum of the args
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum;
+	int i;
+	int x;
+
+	sum = 0;
+	if (n == 0)
+		return (0);
+	va_list args;
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		x = va_arg(args, int);
+		sum += x;
+	}
+	va_end(args);
+	return (sum);
+}
+
