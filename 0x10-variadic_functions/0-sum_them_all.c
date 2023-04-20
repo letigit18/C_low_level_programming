695c7018d70de4994ec41cd49e57fe292b6cf69b
mersigit <mersi.tesfaye18@gmail.com>
2023-04-20 02:19:39 -0700

    calculating the sum is done

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,13 +8,14 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
+	va_list args;
 	unsigned int sum;
 	unsigned int i;
 	unsigned int x;
 
 	sum = 0;
-	va_list args;
-
+	if (n == 0)
+		return (0);
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
