f0f0d66e82975e938c1a7cc4486efdc452f0ab43
mersigit <mersi.tesfaye18@gmail.com>
2023-04-28 03:06:47 -0700

    node element counter

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "lists.h"
+size_t list_len(const list_t *h)
+{
+	size_t count;
+
+	count = 0;
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
