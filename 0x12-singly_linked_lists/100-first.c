24573ac9e6e2bf623005ddb513edffcac0ee5a37
mersigit <mersi.tesfaye18@gmail.com>
2023-04-28 10:35:53 -0700

    100-first.c updated

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,6 +5,7 @@ void print_string(void) __attribute__((constructor));
  */
 void print_string(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
 
