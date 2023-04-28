db2a19cc468e34a6372bc4f54b117fd6c8cc5426
mersigit <mersi.tesfaye18@gmail.com>
2023-04-28 04:07:22 -0700

    adding node done

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "list.h"
+
+/**
+ * add_node - the function that add node
+ * @head: the first parameter
+ * @str: stringn passed to the function
+ * Return: the address of head
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t current;
+	unsigned int length;
+
+	length = 0;
+	while (*str != '\0')
+	{
+		length++;
+	}
+	current = malloc(sizeof(list_t));
+	if (current == NULL)
+	{
+		return (NULL);
+	}
+	current->str = strdup(str);
+	current->len = length;
+	current->next = (*head);
+	(*head) = current;
+	return (*head);
+}
+
+
