50ffa09456c46e4d90bb3771a9e530e4fae97ed2
mersigit <mersi.tesfaye18@gmail.com>
2023-04-28 03:09:08 -0700

    node element counter done

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 #include "lists.h"
+
+/**
+ * list_len - function that returns no. of element
+ * @h: the head node
+ * Return: the no. of element
+ */
 size_t list_len(const list_t *h)
 {
 	size_t count;
