a04f7a1c2e5ebcd19b4d601c49cb04e1e6644f18
mersigit <mersi.tesfaye18@gmail.com>
2023-04-29 23:35:55 -0700

    4-ree_listint.c updated

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint - freeup the memory
+ * @head: the head node
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *ptr;
+
+	while (head != NULL)
+	{
+		ptr = ptr->next;
+		free(head);
+		head = ptr;
+	}
+}
