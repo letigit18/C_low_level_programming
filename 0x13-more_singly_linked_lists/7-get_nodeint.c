0b8f59a64968aa0fd764ca94790c0227031ff785
mersigit <mersi.tesfaye18@gmail.com>
2023-04-30 00:59:30 -0700

    7-get_nodeint.c updated

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -2,14 +2,14 @@
 #include "lists.h"
 
 /**
- * get_nodeint_at_index - get the index of the node 
+ * get_nodeint_at_index - get the index of the node
  * @head: the head node
  * @index: parameter
  * Return: the node at the given index
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint_t *ptr;
+	listint_t *ptr = head;
 	unsigned int c;
 
 	if (head == NULL)
@@ -18,11 +18,11 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	}
 	else
 	{
-	while (head != NULL && c < index)
+	while (ptr != NULL && c < index)
 	{
-		head = head->next;
+		ptr = head->next;
 		c++;
 	}
-	return (head);
+	return (ptr);
 	}
 }
