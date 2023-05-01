d902a21c1c3093314de0d4fffc5690ff7f312de7
mersigit <mersi.tesfaye18@gmail.com>
2023-05-01 06:07:14 -0700

    10-delete_nodeint.c updated

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,7 +12,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	unsigned int pos;
 	listint_t *ptr = *head;
-	listint_t cur = NULL;
+	listint_t *cur = NULL;
 
 	pos = 1;
 	if (*head == NULL)
