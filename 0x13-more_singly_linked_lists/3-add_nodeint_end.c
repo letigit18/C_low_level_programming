c9ae4d47624689b7577cbd82b086363415f0c8fb
mersigit <mersi.tesfaye18@gmail.com>
2023-05-02 02:18:14 -0700

    3-ad_nodeint_end.c updated

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -19,9 +19,9 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		*head = end_node;
 		return (end_node);
 	}
-	while (ptr->next)
+	while (ptr->next != NULL)
 	{
-		ptr  = ptr->next;
+		ptr = ptr->next;
 	}
 	ptr->next = end_node;
 	return (end_node);
