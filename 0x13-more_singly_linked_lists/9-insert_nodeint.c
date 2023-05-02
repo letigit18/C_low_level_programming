28f05b4ebf477553207cca4705395ba420920483
mersigit <mersi.tesfaye18@gmail.com>
2023-05-02 11:18:57 -0700

    9-insert_nodeint.c updated

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,7 +29,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = new_node;
 		return (new_node);
 	}
-	while (ptr && pos < idx - 1)
+	while (ptr && pos < (idx - 1))
 	{
 		pos++;
 		ptr2 = ptr;
