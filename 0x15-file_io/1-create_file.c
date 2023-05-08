f87bf323e5ae7c4c19a47d352c2806430f59cd72
mersigit <mersi.tesfaye18@gmail.com>
2023-05-08 10:21:44 -0700

    1-create_file.c updated

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 			i++;
 		}
 	}
-	fp = open(filename, O_CREAT | O_RDWR | O_TRUNK, 0600);
+	fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	wo = write(fp, text_content, i);
 	if (fp == -1 || wo == -1)
 		return (-1);
