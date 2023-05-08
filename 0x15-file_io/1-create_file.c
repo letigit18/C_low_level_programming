1a76ecec084cf0824db40417c1715292714322c4
mersigit <mersi.tesfaye18@gmail.com>
2023-05-08 10:06:32 -0700

    1-create_file.c updated

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * create_file - the function that creates file
+ * @filename: the name of the file
+ * @text_content: the content of the file
+ * Return: 1 if success or -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fp;
+	int wo;
+	int i;
+
+	i = 0;
+	if (filename == NULL)
+		return (-1);
+	if (text_content != NULL)
+	{
+		while (text_content[i])
+		{
+			i++;
+		}
+	}
+	fp = open(filename, O_CREAT | O_RDWR | O_TRUNK, 0600);
+	wo = write(fp, text_content, i);
+	if (fp == -1 || wo == -1)
+		return (-1);
+	close(fp);
+	return (1);
+}
