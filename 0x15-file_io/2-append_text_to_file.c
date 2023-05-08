a29938a8f956c4e73b482556a73181e078bb73db
mersigit <mersi.tesfaye18@gmail.com>
2023-05-08 10:38:39 -0700

    2-append_text_to_file.c updated

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,7 +14,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	i = 0;
 	if (filename == NULL)
-		return (1);
+		return (-1);
 	if (text_content != NULL)
 	{
 		while (text_content[i])
